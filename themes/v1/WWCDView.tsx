9fa3ad978256f8dadcf73f5439120bc49302359a
Al Mohaimin Farabi <almohaiminfarabi.work@gmail.com>
2026-05-20 23:55:17 +0600

    Update WWCDView.tsx

--- themes/v1/WWCDView.tsx
+++ themes/v1/WWCDView.tsx
@@ -6,12 +6,12 @@ import WidgetStage from "@/components/common/WidgetStage";
 
 export default function WWCDView({ tournamentID }: { tournamentID: string }) {
   const { team, players, ready } = useWWC(tournamentID);
-
+  const [stageReady, setStageReady] = useState(false);
   if (!ready || !team) return null;
 
   return (
-    <WidgetStage dataReady={ready} onReady={animate}>
-      
+    <WidgetStage dataReady={!!data} onReady={() => setStageReady(true)}>
+      <p>s</p>
     </WidgetStage>
   );
 }
